b5d1e30e6b888ba79331e7a907f2392b0c92d1eb
pola8846 <pola8846@gmail.com>
2024-01-12 11:49:15 +0900

    레이저 충돌 처리 등

--- XR-Interaction-Toolkit-Examples-main/Assets/test/ChessLayserCheck.cs
+++ XR-Interaction-Toolkit-Examples-main/Assets/test/ChessLayserCheck.cs
@@ -1,30 +1,120 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChessLayserCheck : MonoBehaviour
 {
     private int pointCount = 0;
+    [SerializeField]
     private bool check = false;
     public bool Check => check;
 
-    public void Point()
+    [SerializeField]
+    private GameObject[] lays;
+    public GameObject[] Lays => lays;
+
+    [SerializeField]
+    private Dictionary<LayserPointer, int> level;
+    public int Level
     {
-        pointCount++;
-        if (pointCount == 1)
+        get
         {
-            check = true;
-            Debug.Log("활성화");
+            int result = -1;
+            foreach (var item in level)
+            {
+                if (item.Value > result)
+                {
+                    result = item.Value;
+                }
+            }
+            return result;
         }
     }
 
-    public void UnPoint()
+    private bool safeSwitch=false;
+
+    private void Start()
     {
-        pointCount--;
-        if (pointCount == 0)
+        foreach (var lay in lays)
+        {
+            lay.GetComponent<LayserPointer>().parent = gameObject;
+        }
+        SetLays(false);
+        level = new();
+    }
+
+    public void Point(LayserPointer layser, int level)
+    {
+        if (SelfCheck(layser))
+        {
+            return;
+        }
+
+        if (Level != -1 && Level <= level)//레벨 체크
+        {
+            return;
+        }
+
+        if (!this.level.ContainsKey(layser))//중복 체크
+        {
+            this.level.Add(layser, level + 1);
+        }
+        else
+        {
+            this.level[layser] = level;
+        }
+
+        if (this.level.Count == 1)//활성화 체크
+        {
+            check = true;
+            Debug.Log("활성화");
+            SetLays(true);
+        }
+    }
+
+    public void UnPoint(LayserPointer layser)
+    {
+        if (SelfCheck(layser))
+        {
+            return;
+        }
+
+        if (level.Count == 1)
         {
             check = false;
             Debug.Log("비활성화");
+            if (safeSwitch==false)
+            {
+                safeSwitch = true;
+                foreach (var lay in lays)
+                {
+                    lay.GetComponent<LayserPointer>().Unpoint();
+                }
+                safeSwitch = false;
+            }
+            
+            SetLays(false);
         }
+        level.Remove(layser);
+    }
+
+    private void SetLays(bool triger)
+    {
+        foreach (var lay in lays)
+        {
+            lay.SetActive(triger);
+        }
+    }
+
+    private bool SelfCheck(LayserPointer layser)
+    {
+        foreach (GameObject lay in lays)
+        {
+            if (lay==layser.gameObject)
+            {
+                Debug.Log(1);
+                return true;
+            }
+        }
+        return false;
     }
 }
